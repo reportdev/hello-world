002203f0cb1f52b0c1cbafea96462f838c7bd204
sravank <sravank@ASRS-2819.asrs.local>
2017-05-11 15:30:45 -0700

    check

---
+++ forloop.java
@@ -0,0 +1,23 @@
+
+public class forloop {
+
+	public static void main(String[] args) {
+
+		int a[] = { 2, 3, 4, 5 };
+		String b[] = { "S", "R", "A", "V", "A", "N" };
+
+		for (int i = 1; i < a.length + 1; i++) {
+
+			System.out.println(a[a.length - i]);
+
+		}
+
+		for (int i = 1; i < b.length + 1; i++) {
+
+			System.out.println(b[b.length - i]);
+
+		}
+
+	}
+
+}
